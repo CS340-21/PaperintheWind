4e484aeda45f5eb08ff047f8dbf3ffc35fd35420
joeylemon <joeyclemon@gmail.com>
2021-02-10 18:59:06 -0500

    remove editor reference to fix build error

--- Assets/Code/Global/Utils.cs
+++ Assets/Code/Global/Utils.cs
@@ -12,10 +12,11 @@ public class Utils
     {
         Debug.LogError(error);
 
-        if (Application.isEditor)
-            UnityEditor.EditorApplication.isPlaying = false;
-        else
-            Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 
     /// <summary>
