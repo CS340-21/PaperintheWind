1d3a2607665cd4715dc47df264f570c2bcebabe8
joeylemon <joeyclemon@gmail.com>
2021-02-21 13:05:06 -0500

    ios quality, fps counter

--- Assets/Code/Player/FPSDisplay.cs
+++ Assets/Code/Player/FPSDisplay.cs
@@ -17,10 +17,10 @@ public class FPSDisplay : MonoBehaviour
 
         GUIStyle style = new GUIStyle();
 
-        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        Rect rect = new Rect(50, 150, w, h * 2 / 100);
         style.alignment = TextAnchor.UpperLeft;
         style.fontSize = h * 2 / 100;
-        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+        style.normal.textColor = new Color(1f, 1f, 1f, 1.0f);
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
         string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
